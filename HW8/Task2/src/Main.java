0bb28758caced78e91584992eaf6e84b10419af4
MansurPoludov <mansurpoludov2@gmail.com>
2023-05-11 19:05:36 +0300

    создал массив с циклом в котором вывел средний вес людей и количество людей с весом от 60 до 100 кг

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+       double [] weight = new double [10];
+        double middleWeight = 0 ;
+        int counter = 0;
+        for(int i = 0 ; i < weight.length ; i++ ) {
+       weight [i] = new Random().nextDouble(40 , 100);
+
+            middleWeight += weight[i] / weight.length ;
+
+            if( weight[i] >= 60 && weight[i] <= 100 ) {
+                counter++ ;
+        }
+            System.out.println(weight[i]);
+        }
+
+        System.out.println("Средний вес 10 человек - " + middleWeight + " кг");
+        System.out.println("Количество людей в весовой категории от 60 до 100кг - " + counter + " человек");
     }
 }
